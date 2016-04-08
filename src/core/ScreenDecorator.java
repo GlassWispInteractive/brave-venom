e1dfc9156f544fbd7bf3aa901bcb7a5742c30905
Danny Rademacher <danny.rademacher@gmail.com>
2016-04-08 12:28:21 +0200

    import from fictional-spoon

---
+++ src/core/ScreenDecorator.java
@@ -0,0 +1,21 @@
+package core;
+
+public abstract class ScreenDecorator extends Screen {
+	protected Screen decoratedScreen;
+	
+	public ScreenDecorator(Screen decoratedScreen) {
+		this.decoratedScreen = decoratedScreen;
+		group = decoratedScreen.group;
+		scene = decoratedScreen.scene;
+		layers = decoratedScreen.layers;
+		gcs = decoratedScreen.gcs;
+	}
+	
+	protected void tick(int ticks) {
+		decoratedScreen.tick(ticks);
+	}
+
+	protected void render() {
+		decoratedScreen.render();
+	}
+}
