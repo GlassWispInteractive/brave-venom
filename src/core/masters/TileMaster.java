14d88be85f960872a8fc8c34b32bf5fd5cbb8759
Danny <danny.rademacher@gmail.com>
2016-04-15 20:49:51 +0200

    single images instead of tile sheets

--- src/core/masters/TileMaster.java
+++ src/core/masters/TileMaster.java
@@ -1,10 +1,18 @@
 package core.masters;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map.Entry;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
@@ -21,20 +29,83 @@ public class TileMaster {
 	public TileMaster() {
 		// load all tile sheets from the specific resource folder
 		try {
-			Files.walk(Paths.get("res/tilesheet")).forEach(filePath -> {
-				if (Files.isRegularFile(filePath)) {
-					int start = filePath.toString().lastIndexOf(System.getProperty("file.separator")) + 1;
-					int end = filePath.toString().lastIndexOf(".");
+			Files.walk(Paths.get("res/tilesheet")).filter(path -> !path.toString().endsWith(".xml"))
+					.forEach(filePath -> {
+						if (Files.isRegularFile(filePath)) {
+							int start = filePath.toString().lastIndexOf(System.getProperty("file.separator")) + 1;
+							int end = filePath.toString().lastIndexOf(".");
 
-					String name = filePath.toString().substring(start, end);
+							String name = filePath.toString().substring(start, end);
+							File xml = new File(filePath.toString().substring(0, end) + ".xml");
 
-					// save the full image
-					sheets.put(name, new Image(filePath.toUri().toString()));
-				}
-			});
+							// save the full image
+							if (xml.exists()) {
+
+								try {
+									DOMParser parser = new DOMParser();
+									parser.parse(xml.toURI().toString());
+									Document doc = parser.getDocument();
+
+									// Get the document's root XML node
+									
+
+									Node main = doc.getFirstChild();
+									
+									
+									
+									
+									NodeList tiles = main.getChildNodes();
+									
+									for (int i = 0; i < tiles.getLength(); i++) {
+										Node node = tiles.item(i);
+										System.out.println(node.getAttributes().getNamedItem("height"));
+										
+
+//										for (int j = 0; j < attr.getLength(); j++) {
+//											System.out.println(attr.getNamedItem("height"));
+//										}
+									}
+
+									// Navigate down the hierarchy to get to the
+									// CEO node
+									// Node comp = getNode("Company", root);
+									// Node exec = getNode("Executive",
+									// comp.getChildNodes() );
+									// String execType = getNodeAttr("type",
+									// exec);
+
+									// Load the executive's data from the XML
+									// NodeList nodes = exec.getChildNodes();
+									// String lastName =
+									// getNodeValue("LastName", nodes);
+									// String firstName =
+									// getNodeValue("FirstName", nodes);
+									// String street = getNodeValue("street",
+									// nodes);
+									// String city = getNodeValue("city",
+									// nodes);
+									// String state = getNodeValue("state",
+									// nodes);
+									// String zip = getNodeValue("zip", nodes);
+									//
+									// System.out.println(lastName + ", " +
+									// firstName);
+									// System.out.println(street);
+									// System.out.println(city + ", " + state +
+									// " " + zip);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+
+								sheets.put(name, new Image(filePath.toUri().toString()));
+							}
+						}
+					});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		System.exit(0);
 
 		for (Entry<String, Image> e : sheets.entrySet()) {
 			Image sheet = e.getValue();
@@ -67,5 +138,72 @@ public class TileMaster {
 		tiles.get(key)[tileX][tileY] = newImage;
 
 		return newImage;
+	}
+
+	// ...
+
+	protected Node getNode(String tagName, NodeList nodes) {
+		for (int x = 0; x < nodes.getLength(); x++) {
+			Node node = nodes.item(x);
+			if (node.getNodeName().equalsIgnoreCase(tagName)) {
+				return node;
+			}
+		}
+
+		return null;
+	}
+
+	protected String getNodeValue(Node node) {
+		NodeList childNodes = node.getChildNodes();
+		for (int x = 0; x < childNodes.getLength(); x++) {
+			Node data = childNodes.item(x);
+			if (data.getNodeType() == Node.TEXT_NODE)
+				return data.getNodeValue();
+		}
+		return "";
+	}
+
+	protected String getNodeValue(String tagName, NodeList nodes) {
+		for (int x = 0; x < nodes.getLength(); x++) {
+			Node node = nodes.item(x);
+			if (node.getNodeName().equalsIgnoreCase(tagName)) {
+				NodeList childNodes = node.getChildNodes();
+				for (int y = 0; y < childNodes.getLength(); y++) {
+					Node data = childNodes.item(y);
+					if (data.getNodeType() == Node.TEXT_NODE)
+						return data.getNodeValue();
+				}
+			}
+		}
+		return "";
+	}
+
+	protected String getNodeAttr(String attrName, Node node) {
+		NamedNodeMap attrs = node.getAttributes();
+		for (int y = 0; y < attrs.getLength(); y++) {
+			Node attr = attrs.item(y);
+			if (attr.getNodeName().equalsIgnoreCase(attrName)) {
+				return attr.getNodeValue();
+			}
+		}
+		return "";
+	}
+
+	protected String getNodeAttr(String tagName, String attrName, NodeList nodes) {
+		for (int x = 0; x < nodes.getLength(); x++) {
+			Node node = nodes.item(x);
+			if (node.getNodeName().equalsIgnoreCase(tagName)) {
+				NodeList childNodes = node.getChildNodes();
+				for (int y = 0; y < childNodes.getLength(); y++) {
+					Node data = childNodes.item(y);
+					if (data.getNodeType() == Node.ATTRIBUTE_NODE) {
+						if (data.getNodeName().equalsIgnoreCase(attrName))
+							return data.getNodeValue();
+					}
+				}
+			}
+		}
+
+		return "";
 	}
 }
